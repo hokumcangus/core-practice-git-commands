0e2bd087dc72ad5617b6cc10163a590c54930f10
hoku mcangus <mcangusohana2@gmail.com>
2022-09-21 22:44:20 -0700

    Passes test

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import pytest
 
 
 def always_returns_true():
-    return False
+    a = "hoku"
+    print(a)
+    return True
 
 
 def test_always_returns_true():
     assert always_returns_true()
+    
+always_returns_true()
+test_always_returns_true()
